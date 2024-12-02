c52948992989aaf784f36af0c6f29c5bc9d767a2
Nabe <vohongduc000@gmail.com>
2024-12-02 20:54:26 +0700

    finish part 3

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Chats from "./Chats";
-import { Stack } from "@mui/material";
+import { Stack, useTheme } from "@mui/material";
 import { Box } from "@mui/material";
 import { Conversation } from "../../components/conversation";
 
 const GeneralApp = () => {
+  const theme = useTheme();
   return (
     <Stack direction="row" sx={{ width: "100%" }}>
       {/* Chat */}
@@ -14,7 +15,7 @@ const GeneralApp = () => {
         sx={{
           height: "100%",
           width: "calc(100vw - 420px)",
-          backgroundColor: "#fff",
+          backgroundColor: theme.palette.mode === "light" ? "#fff": theme.palette.background.default,
         }}
       >
         {/* Conversation */}
